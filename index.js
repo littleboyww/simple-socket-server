e8a20197fe101dca49a53a753ddf158f150cc54e
littleboyww <148840840+littleboyww@users.noreply.github.com>
2023-11-17 04:04:09 +0000

    Add simple handle for message emit

--- index.js
+++ index.js
@@ -23,10 +23,11 @@ publishActionNs.on("connection", (socket) => {
       deviceId: deviceId,
       deviceName: deviceName
     }
-    if(devices[deviceId]) {
+    if(devices[socket.id]) {
       callback({success: false, errorMsg: "Fail to init session!! Existed the connection with this device"})
     }
-    devices[deviceId] = saved
+    
+    devices[socket.id] = saved
     console.log("saved:log:" + devices)
     callback({success: true})
   })
@@ -34,51 +35,82 @@ publishActionNs.on("connection", (socket) => {
   socket.on('createRoom', (data, callback) => {
     console.log(`createRoom:socketId:${socket.id} - data: ${data.toString()}`)
     const {deviceId, roomName} = data
-    if(!devices[deviceId]) {
+    if(!devices[socketId]) {
       callback({success: false, errorMsg: "Invalid credentials error!! Fail to create new Room"})
     }
+    
     if(rooms[roomName]) {
       callback({success: false, errorMsg: "Room name existed!!"})
       return
     }
     publishActionNs.to(socket.id).socketsJoin(`/rooms/${roomName}`)
     rooms[roomName] = publishActionNs.to(`/rooms/${roomName}`)
-    console.log("saved:log:" + devices)
+    console.log(`createRoom:savedRoom:${rooms}`)
     callback({success: true})
   })
 
   socket.on("joinRoom", (data, callback) => {
     console.log(`joinRoom:socketId:${socket.id} - data: ${data.toString()}`)
     const {deviceId, roomName} = data
-    if(!devices[deviceId]) {
+
+    if(!devices[socket.id]) {
+      console.log(`joinRoom:socketId:${socket.id}:callback - Invalid credentials error!! Fail to create new Room`)
       callback({success: false, errorMsg: "Invalid credentials error!! Fail to create new Room"})
+    } else {
+      if(!getDeviceByDeviceId(deviceId)) {
+        console.log(`joinRoom:socketId:${socket.id}:callback - Invalid credentials error!! Fail to create new Room`)
+        callback({success: false, errorMsg: "Invalid credentials error!! Fail to create new Room"})
+        return
+      }
     }
     if(!rooms[roomName]) {
+      console.log(`joinRoom:socketId:${socket.id}:callback - Not found room!!`)
       callback({success: false, errorMsg: "Not found room!!"})
       return
     }
     publishActionNs.to(socket.id).socketsJoin(roomName)
-    rooms[roomName].emit(`message`, { type: "USER_JOINED_EVENT", deviceId: deviceId, deviceName: devices[deviceId].deviceName })
+    rooms[roomName].emit(`message`, { type: "USER_JOINED_EVENT", deviceId: devices[socket.id].deviceId, deviceName: devices[socket.id].deviceName })
+    console.log(`joinRoom:socketId:${socket.id}:callback - success`)
+    callback({success: true})
   })
 
   socket.on("leaveRoom", (data) => {
     const { deviceId, roomName } = data
+    handleLeaveRoom(deviceId, roomName)
+  
+  })
+
+  socket.on('disconnect', () => {
+    const deviceId = devices[socket.id].deviceId
+    const inRooms = Object.keys(socket.rooms)
+    inRooms.forEach((room) => {
+      console.log("disconnect:room:" + room)
+      handleLeaveRoom(deviceId, room)
+    })
+  })
+
+
+  const handleLeaveRoom = (deviceId, roomName) => {
     if(rooms[roomName]) {
-      socket.leave(`/rooms/${roomName}`)
+      socket.leave((roomName))
       const roomClients = rooms[roomName].clients
       if(roomClients.length === 0) {
         delete rooms[roomName]
       } else {
-        rooms[roomName].emit("message", { type: "USER_LEAVED_EVENT", deviceId: deviceId, deviceName: devices[deviceId].deviceName })
+        socket.to(roomName).emit('message', {type: "USER_LEAVED_EVENT", deviceId: deviceId, deviceName: devices[deviceId].deviceName})
       }
     }
-  })
+  }
+})
 
-  socket.on('disconnecting', () => {
-    console.log(`disconnecting:socketId:${socket.id}`)
-    let inRooms = Object.keys(socket.rooms)
-    inRooms.forEach((room) => {
-      socket.to(room).emit("message", { type: "USER_" })
-    })
-  })
+const getDeviceByDeviceId = (deviceId) => {
+  return Object.values(devices).find(device => device.deviceId === deviceId)
+}
+
+io.on("connection", (socket) => {
+  console.log("Connect socket: " + socket.id)
+})
+const PORT = process.env.PORT || 3000
+httpServer.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
 })
